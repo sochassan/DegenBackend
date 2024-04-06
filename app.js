6a986dc507eb8f7255de7edb60e60451373b199c
sochassan <a.hassan.asif@soc-solution.com>
2024-04-07 03:20:36 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+mongoose.connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+    next();
+});
+
+
+// controllers
+const UserController = require('./controllers/user');
+
+// routes
+app.get('/getuser', UserController.getUser);
+app.get('/getallusers', UserController.getAllAddressesWithEntryCount);
+
+app.post('/addreferral', UserController.addReferral);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ controllers/user.js
@@ -0,0 +1,56 @@
+const User = require('../models/user');
+
+class UserController {
+    async getUser(req, res) {
+      try {
+        const user = req.query.walletAddress;
+    
+        // const user = await User.findOne({ walletAddress });
+    
+        if (!user) {
+          return res.json({ success: false, message: 'User does not exist' });
+        }
+  
+        const totelReferral = await User.countDocuments({ user });
+    
+        return res.json({ success: true, user, totelReferral });
+      } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+      }
+    }
+  
+
+    async addReferral(req, res) {
+      try {
+        const { walletAddress, referralAddress } = req.body;
+        const existingUser = await User.findOne({ referralAddress, walletAddress });
+    
+        if (existingUser) {
+          return res.json({ success: false, message: 'Referral address is already associated with this wallet address' });
+        }
+        const newUser = new User({ walletAddress, referralAddress });
+        await newUser.save();
+    
+        return res.json({ success: true, message: 'Referral address added successfully to the wallet address' });
+      } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+      }
+    }
+    async getAllAddressesWithEntryCount(req, res) {
+      try {
+        const addressCounts = await User.aggregate([
+          { $group: { _id: "$walletAddress", count: { $sum: 1 } } }
+        ]);
+    
+        // Return the result
+        return res.json({ success: true, addressCounts });
+      } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+      }
+    }
+    
+
+ 
+}
+
+module.exports = new UserController();
