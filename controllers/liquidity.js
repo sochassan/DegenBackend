3a6380864016091a486af0b41557c1fd4d0f010e
Ammar-Hussain-Siddiqui <ammar.siddiqui@soc-solution.com>
2024-04-19 18:38:07 +0500

    modify saveLiquidity api to handle same token liquidity

--- controllers/liquidity.js
+++ controllers/liquidity.js
@@ -4,9 +4,33 @@ class LiquidityController {
 
     async saveLiquidity(req, res) {
         try {
-            let data = req.body
-            const newLiquidity = new Liquidity({ ...data });
-            const response = await newLiquidity.save()
+            let data = req.body;
+            console.log(data)
+            const filter = {
+                public_key: data.public_key,
+                fromTokenName: data.fromTokenName,
+                ToTokenName: data.ToTokenName
+            };
+            const update = {
+                $inc: {
+                    fromTokenValue: data.fromTokenValue,
+                    ToTokenValue: data.ToTokenValue
+                },
+                $setOnInsert: {
+                    fromToken: data.fromToken,
+                    ToToken: data.ToToken,
+                    public_key: data.public_key,
+                    fromTokenName: data.fromTokenName,
+                    ToTokenName: data.ToTokenName
+                }
+            };
+            const options = {
+                new: true,
+                upsert: true
+            };
+
+            const response = await Liquidity.findOneAndUpdate(filter, update, options);
+
             return res.status(200).json({ success: true, data: response });
         } catch (error) {
             return res.status(500).json({ success: false, message: error.message });
@@ -18,7 +42,7 @@ class LiquidityController {
             if (!public_key) {
                 return res.status(400).json({ success: false, message: "Public key is required" });
             }
-            const liquidityData = await Liquidity.find({ public_key });
+            const liquidityData = await Liquidity.find({ public_key })
             return res.status(200).json({ success: true, data: liquidityData });
 
         } catch (error) {
@@ -29,7 +53,7 @@ class LiquidityController {
     async deleteLiquidity(req, res) {
         try {
             let { id } = req.query;
-            if (!public_key) {
+            if (!id) {
                 return res.status(400).json({ success: false, message: "Liquidity id is required" });
             }
             const deletedLiquidity = await Liquidity.findByIdAndDelete(id);
